78c5d80007200810bdb814c86ca89978ad3d269e
AsmaAlfauri <51743787+AsmaAlfauri@users.noreply.github.com>
2019-09-04 09:50:20 +0300

    first pull req

--- servers/events.js
+++ servers/events.js
@@ -7,18 +7,17 @@ router.get("/", (req, res) => {
 });
 // localhost9000/events/
 // get request with database
-router.post('/a', (req, res) => {
-  console.log('SERVER:Get Data','get Events from server');
-  mongo.createEvents((result) => {
+router.post("/a", (req, res) => {
+  console.log("SERVER:Get Data", "get Events from server");
+  mongo.createEvents(result => {
     res.json(result);
-  })
+  });
 });
 
-
-router.get('/Events', (req, res) => {
-  console.log('SERVER:Get Data','get Events from server');
-  mongo.getEvents((result) => {
+router.get("/Events", (req, res) => {
+  console.log("SERVER:Get Data", "get Events from server");
+  mongo.getEvents(result => {
     res.json(result);
-  })
+  });
 });
 module.exports = router;
