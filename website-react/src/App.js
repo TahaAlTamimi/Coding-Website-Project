4fb83dd91f6adc490937b3f6d182c7de8e7173b3
TahaAlTamimi <altamimitmo@gmail.com>
2019-09-04 09:36:51 +0300

    added card without design nav bar

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,23 +1,57 @@
-import React, { Component } from "react";
-import "./App.css";
-import axios from "axios";
+import React, { PureComponent } from 'react'
+import Header from './component/Header'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import MaterialCode from './component/MaterialCode'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+// import HTMLBasic from './component/TransferToMaterial/HTMLBasic'
+// import HTMLIntermediate from './component/TransferToMaterial/HTMLIntermediate'
+// import HTMLAdvanced from './component/TransferToMaterial/HTMLAdvanced'
+// import CssBasic from './component/TransferToMaterial/CssBasic'
+// import CssIntermediate from './component/TransferToMaterial/CssIntermediate'
+// import CssAdvanced from './component/TransferToMaterial/CssAdvanced'
+// import JSBasic from './component/TransferToMaterial/JSBasic'
+// import JSIntermediate from './component/TransferToMaterial/JSIntermediate'
+// import JSAdvance from './component/TransferToMaterial/JSAdavance'
 
-class App extends Component {
-  state = {};
-
-  checkServer = () => {
-    axios.get("http://localhost:9000/posts/").then(res => {
-      console.log(res.data);
-    });
-  };
+export class App extends PureComponent {
+  state={HeaderShow:true}
   render() {
     return (
-      <>
-        <h1>Working Fine</h1>
-        <button onClick={this.checkServer}>Checking server</button>
-      </>
-    );
+      
+      <div>
+       <Router>
+       <Header/>
+                
+                <Switch>
+                 <Route path="/" exact component={Home}/>
+                 <Route  path="/material" exact component={MaterialCode}/>
+                  {/* <Route path="/material/0" component={HTMLBasic}/>
+                  <Route path="/material/1" component={HTMLIntermediate}/>
+                  <Route path="/material/2" component={HTMLAdvanced}/>
+
+                  <Route path="/material/3" component={CssBasic}/>
+                  <Route path="/material/4" component={CssIntermediate}/>
+                  <Route path="/material/5" component={CssAdvanced}/>
+                  <Route path="/material/6" component={JSBasic}/>
+                  <Route path="/material/7" component={JSIntermediate}/>
+                  <Route path="/material/8" component={JSAdvance}/> */}
+                </Switch>
+                
+               
+         </Router>
+      
+       {/* < /> */}
+      
+      </div>
+      
+    )
   }
 }
+const Home =()=>(
+  <div>
+    <h1>Home page</h1>
+    </div>
 
-export default App;
+)
+export default App
+
