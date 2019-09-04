6d9657e6a865abbf0d876c4e9a8b80692a82eccc
AhmadSharafedin <sharafedin.ahmad@gmail.com>
2019-09-04 10:20:58 +0300

    FE sign in sign up

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+import AppSharafalden from "./components/AppSharafalden";
 import axios from "axios";
 
 class App extends Component {
   state = {};
 
   checkServer = () => {
-    axios.get("http://localhost:9000/posts/").then(res => {
-      console.log(res.data);
-    });
+   axios.post("http://localhost:9000/signIn/sign-in")
+   .then( res => {
+     console.log(res.data)
+   })
   };
   render() {
     return (
       <>
         <h1>Working Fine</h1>
         <button onClick={this.checkServer}>Checking server</button>
+        <AppSharafalden/>
       </>
     );
   }
---
+++ website-react/src/components/AppSharafalden.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
+
+
+
+
+class AppSharafalden extends Component {
+  render() {
+    return (
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="App__Aside">
+            <h1>Welcome to Orange Coding Academy</h1>
+          </div>
+
+          <div className="App__Form">
+            <div className="FormTitle">
+              <NavLink
+                to="/sign-in"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign In
+              </NavLink>{" "}
+              or{" "}
+              <NavLink
+                exact
+                to="/"
+                activeClassName="FormTitle__Link--Active"
+                className="FormTitle__Link"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpForm}></Route>
+            <Route path="/sign-in" component={SignInForm}></Route>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default AppSharafalden;
